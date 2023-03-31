74a0a71528a421c4f5cb9f77ee88c5556fc1f597
kimi1723 <100174940+kimi1723@users.noreply.github.com>
2023-03-31 19:17:15 +0200

    mobile

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,1 +1,10 @@
-console.log('works');
+const todoInput = document.querySelector('.todo-input');
+const alertInfo = document.querySelector('.alert-info');
+const addBtn = document.querySelector('.add-btn');
+const ulList = document.querySelector('.todo-list ul');
+const allTasks = document.getElementsByTagName('li');
+const popup = document.querySelector('.popup');
+const popupInfo = document.querySelector('.popup-info');
+const popupInput = document.querySelector('.popup-input');
+const addPopupBtn = document.querySelector('.accept');
+const closeTodoBtn = document.querySelector('.cancel');
