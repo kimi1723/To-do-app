100a0f73e67bcdbe145119fc02031c9132bd166c
kimi1723 <100174940+kimi1723@users.noreply.github.com>
2023-04-02 15:08:16 +0200

    adding assignment logic

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,10 +1,41 @@
-const todoInput = document.querySelector('.todo-input');
-const alertInfo = document.querySelector('.alert-info');
-const addBtn = document.querySelector('.add-btn');
-const ulList = document.querySelector('.todo-list ul');
-const allTasks = document.getElementsByTagName('li');
-const popup = document.querySelector('.popup');
-const popupInfo = document.querySelector('.popup-info');
-const popupInput = document.querySelector('.popup-input');
-const addPopupBtn = document.querySelector('.accept');
-const closeTodoBtn = document.querySelector('.cancel');
+const toDoAddBtn = document.querySelector('.to-do-header__add-button') as HTMLButtonElement;
+const toDoInput = document.querySelector('.to-do-header__input') as HTMLInputElement;
+let toDoAssignmentID: number = 1;
+
+const validateToDo = (): boolean => {
+	const toDoError = document.querySelector('.to-do-header__error') as HTMLParagraphElement;
+
+	if (toDoInput.value === '') {
+		toDoError.classList.add('to-do-header__error--active');
+		toDoError.classList.add('to-do-header__error--active-visibility');
+		return false;
+	} else {
+		toDoError.classList.remove('to-do-header__error--active');
+		setTimeout(() => {
+			toDoError.classList.remove('to-do-header__error--active-visibility');
+		}, 300);
+		return true;
+	}
+};
+
+const createToDoBody = (): void => {
+	const template = document.querySelector('.to-do-assignment-template') as HTMLTemplateElement;
+	const toDoAssignmentsContainer = document.querySelector('.to-do-assignments') as HTMLDivElement;
+	const toDoAssignment = template.content.cloneNode(true) as HTMLElement;
+	const toDoAssignmentText = toDoAssignment.querySelector('.to-do-assignment__text') as HTMLParagraphElement;
+	const toDoAssignmentContainer = toDoAssignment.querySelector('.to-do-assignment') as HTMLDivElement;
+
+	toDoAssignmentContainer.dataset.id = toDoAssignmentID.toString();
+
+	toDoAssignmentText.textContent = toDoInput.value;
+
+	toDoAssignmentsContainer.append(toDoAssignment);
+	toDoAssignmentID++;
+};
+
+const addToDo = (): void => {
+	if (validateToDo() === false) return;
+	createToDoBody();
+};
+
+toDoAddBtn.addEventListener('click', addToDo);
